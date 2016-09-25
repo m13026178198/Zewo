b508cd8a48ba7e2b726dd9116fa9c5a2d9b30715
Robert Payne <robertpayne@me.com>
2016-09-25 21:19:12 +1300

    Patch OpenSSL hash and add PBKDF

--- Modules/OpenSSL/Sources/OpenSSL/Hash.swift
+++ Modules/OpenSSL/Sources/OpenSSL/Hash.swift
@@ -70,9 +70,9 @@ public struct Hash {
 		initialize()
         
         let messageBuffer = message.buffer
-        return Buffer(count: function.digestLength) { bytesBuffer in
-            _ = messageBuffer.withUnsafeBytes { (messageBytes: UnsafePointer<UInt8>) in
-                function.function(messageBytes, messageBuffer.count, bytesBuffer.baseAddress!)
+        return Buffer(count: function.digestLength) { bufferPtr in
+            _ = messageBuffer.withUnsafeBufferPointer { (messageBufferPtr: UnsafeBufferPointer<UInt8>) in
+                function.function(messageBufferPtr.baseAddress!, messageBufferPtr.count, bufferPtr.baseAddress!)
             }
         }
 	}
@@ -82,25 +82,54 @@ public struct Hash {
 	public static func hmac(_ function: Function, key: BufferRepresentable, message: BufferRepresentable) -> Buffer {
 		initialize()
         
-        let keyBuffer = key.buffer
+        var keyBuffer = key.buffer
         let messageBuffer = message.buffer
         
-        return Buffer(capacity: Int(EVP_MAX_MD_SIZE)) { bytesBuffer in
-            return keyBuffer.withUnsafeBytes { (keyBytes: UnsafePointer<UInt8>) -> Int in
-                return messageBuffer.withUnsafeBytes { (messageBytes: UnsafePointer<UInt8>) -> Int in
+        let blockSize = Int(EVP_MD_block_size(function.evp))
+        if keyBuffer.count < blockSize {
+            keyBuffer.append(Buffer([UInt8](repeating: 0, count: blockSize - keyBuffer.count)))
+        }
+        
+        return Buffer(capacity: Int(EVP_MAX_MD_SIZE)) { bufferPtr in
+            return keyBuffer.withUnsafeBufferPointer { (keyBufferPtr: UnsafeBufferPointer<UInt8>) -> Int in
+                return messageBuffer.withUnsafeBufferPointer { (messageBufferPtr: UnsafeBufferPointer<UInt8>) -> Int in
                     var outLength: UInt32 = 0
                     _ = COpenSSL.HMAC(function.evp,
-                                  keyBytes,
-                                  Int32(keyBuffer.count),
-                                  messageBytes,
-                                  messageBuffer.count,
-                                  bytesBuffer.baseAddress!,
-                                  &outLength)
+                                      keyBufferPtr.baseAddress,
+                                      Int32(keyBufferPtr.count),
+                                      messageBufferPtr.baseAddress,
+                                      messageBufferPtr.count,
+                                      bufferPtr.baseAddress,
+                                      &outLength)
                     return Int(outLength)
                 }
             }
         }
-	}
+    }
+    
+    // MARK: PBKDF
+    
+    public static func pbkdf2(_ function: Function, password: BufferRepresentable, salt: BufferRepresentable, iterations: Int) throws -> Buffer {
+        initialize()
+        
+        let passwordBuffer = password.buffer
+        let saltBuffer = salt.buffer
+        
+        return Buffer(count: Int(function.digestLength)) { bufferPtr in
+            passwordBuffer.withUnsafeBufferPointer { (passwordBufferPtr: UnsafeBufferPointer<Int8>) in
+                saltBuffer.withUnsafeBufferPointer { (saltBufferPtr: UnsafeBufferPointer<UInt8>) in
+                    _ = COpenSSL.PKCS5_PBKDF2_HMAC(passwordBufferPtr.baseAddress,
+                                                   Int32(passwordBufferPtr.count),
+                                                   saltBufferPtr.baseAddress,
+                                                   Int32(saltBufferPtr.count),
+                                                   Int32(iterations),
+                                                   function.evp,
+                                                   Int32(bufferPtr.count),
+                                                   bufferPtr.baseAddress)
+                }
+            }
+        }
+    }
 
 	// MARK: - RSA
 
